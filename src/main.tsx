7318aa20235025abe73c7aeb34a068f5c2a7b7a3
Dusk <91294230+MoonDusk1996@users.noreply.github.com>
2023-01-12 06:06:41 -0400

    feat: welcome console

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,8 @@ import { App } from "./app";
 
 import "./index.css";
 
+console.log(
+  "Seja muito bem vindo!\nPagina desenvolvida com carinho por:\n\n   🎀[̲̅D][̲̅u][̲̅s][̲̅k]🎀   \n\nVisite meu portfólio em:\nhttps://github.com/MoonDusk1996"
+);
+
 render(<App />, document.getElementById("app") as HTMLElement);
